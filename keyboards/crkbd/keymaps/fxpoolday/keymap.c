2c63e6d88409c0f623fff152e0fb1c1787cd4001
fxpoolday <154590208+fxpoolday@users.noreply.github.com>
2023-12-26 04:31:09 +0100

    added screenshot combo

--- keyboards/crkbd/keymaps/fxpoolday/keymap.c
+++ keyboards/crkbd/keymaps/fxpoolday/keymap.c
@@ -34,23 +34,25 @@ const uint16_t PROGMEM vb_combo[] = {KC_V, KC_B, COMBO_END};
 const uint16_t PROGMEM cv_combo[] = {KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM zx_combo[] = {KC_Z, KC_X, COMBO_END};
 const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM as_combo[] = {KC_A, KC_S, COMBO_END};
+const uint16_t PROGMEM asf_combo[] = {KC_A, KC_S, KC_D, COMBO_END};
 const uint16_t PROGMEM hj_combo[] = {KC_H, KC_J, COMBO_END};
 const uint16_t PROGMEM jkl_combo[] = {KC_J, KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM sdf_combo[] = {KC_S, KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM yu_combo[] = {KC_Y, KC_U, COMBO_END};
 const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
 const uint16_t PROGMEM dotcom_combo[] = {KC_COMM, KC_DOT, COMBO_END};
+
 combo_t key_combos[] = {
   COMBO(vb_combo, LCMD(KC_V)),
   COMBO(cv_combo, LCMD(KC_C)),
   COMBO(zx_combo, LCMD(KC_Z)),
-  COMBO(as_combo, SCREENSHOT),
+  COMBO(asf_combo, SCREENSHOT),
   COMBO(hj_combo, LCTL(KC_ENT)),
   COMBO(jkl_combo, MOVE_SCREEN_RIGHT),
   COMBO(sdf_combo, MOVE_SCREEN_LEFT),
   COMBO(dotcom_combo, KC_UNDS),
 };
+
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
